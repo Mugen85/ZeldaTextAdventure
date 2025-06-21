a859c59bee138e5aacd2bb9ed0681f690fdecfbb
MarcoMorelloDev <doppiam1@hotmail.it>
2025-06-21 17:05:50 +0200

    feat: Aggiungi modelli di dati e file JSON per la migrazione
    Prepara il progetto alla migrazione dei dati di gioco da file .txt a un unico file JSON.
    
    - Creata una nuova classe `GameData` in `Models` per mappare l'intera struttura del file JSON.
    - Utilizzati gli attributi `JsonPropertyName` per garantire un corretto mapping tra JSON e le proprietà C#.
    - Creato il file `Data/GameData.json` che centralizza tutti i dati del mondo di gioco (storia, stanze, mostri), precedentemente distribuiti in più file di testo.
    - In questa fase, i nuovi file sono stati aggiunti al progetto ma non sono ancora utilizzati dalla logica di caricamento del gioco, che rimane funzionante con il vecchio sistema.

---
+++ Models/GameData.cs
@@ -0,0 +1,30 @@
+﻿// In Models/GameData.cs
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace ZeldaTextAdventure.Models
+{
+    // Questa è la classe principale che rappresenta l'intero file JSON
+    public class GameData
+    {
+        // Usiamo un attributo per far corrispondere la chiave "startStory" del JSON
+        // alla proprietà "StartStory" di C#, seguendo le convenzioni di nomenclatura.
+        [JsonPropertyName("startStory")]
+        public string? StartStory { get; set; }
+
+        [JsonPropertyName("endWin")]
+        public string? EndWin { get; set; }
+
+        [JsonPropertyName("endLose")]
+        public string? EndLose { get; set; }
+
+        [JsonPropertyName("endDead")]
+        public string? EndDead { get; set; }
+
+        [JsonPropertyName("monsters")]
+        public List<Monster>? Monsters { get; set; }
+
+        [JsonPropertyName("rooms")]
+        public List<Room>? Rooms { get; set; }
+    }
+}
